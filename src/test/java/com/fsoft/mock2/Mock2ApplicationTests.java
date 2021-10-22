21b7f82ba3de945b35b8fc68bb79da3c253b4e93
HaHust <vanha.hust@gmail.com>
2021-10-22 15:23:54 +0700

    * create project structure

---
+++ src/test/java/com/fsoft/mock2/Mock2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsoft.mock2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mock2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
