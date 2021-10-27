c4cf54afd167c81ab1cd94f16d192efbb94dca49
TrangTLH <huyentrangtx86@gmail.com>
2021-10-27 15:18:21 +0700

    - remove file User entity

--- src/main/java/com/fsoft/mock2/controller/QuestionController.java
+++ src/main/java/com/fsoft/mock2/controller/QuestionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("api/v1/question")
 public class QuestionController {
 
     @Autowired
